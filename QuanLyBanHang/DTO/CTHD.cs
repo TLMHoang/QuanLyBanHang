ea281c476b0c7dbba8be52c127f9230daca4b254
fsminhhoangfs <fsminhhoangfs@gmail.com>
2020-01-04 01:04:16 +0700

    Create Project

---
+++ QuanLyBanHang/DTO/CTHD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class CTHD
+    {
+        public int IDHoaDon { get; set; }
+        public int IDHang { get; set; }
+        public int SoLuong { get; set; }
+
+
+        public CTHD() { }
+        public CTHD(int iDHoaDon, int iDHang, int soLuong, int tong)
+        {
+            IDHoaDon = iDHoaDon;
+            IDHang = iDHang;
+            SoLuong = soLuong;
+        }
+
+        public CTHD(DataRow dr)
+        {
+            IDHoaDon = Convert.ToInt32(dr["IDHoaDon"]);
+            IDHang = Convert.ToInt32(dr["IDHang"]);
+            SoLuong = Convert.ToInt32(dr["SoLuong"]);
+        }
+    }
+}
---
+++ QuanLyBanHang/DTO/TaiKhoan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    class TaiKhoan
+    {
+        public int ID { get; set; }
+        public string TKhoan { get; set; }
+        public string MatKhau { get; set; }
+        public int KeyMaHoa { get; set; }
+        public byte Loai { get; set; }
+
+        public TaiKhoan() { ID = -1; TKhoan = ""; MatKhau = ""; KeyMaHoa = new Random().Next(100, 999); Loai = 1; }
+
+        public TaiKhoan(int iD, string tKhoan, string matKhau, int keyMaHoa, byte loai)
+        {
+            ID = iD;
+            TKhoan = tKhoan;
+            MatKhau = matKhau;
+            KeyMaHoa = keyMaHoa;
+            Loai = loai;
+        }
+
+        public TaiKhoan(DataRow dr)
+        {
+            ID = Convert.IsDBNull(dr["ID"]) ? -1 : Convert.ToInt32(dr["ID"]);
+            TKhoan = dr["TaiKhoan"].ToString();
+            MatKhau = dr["MatKhau"].ToString(); ;
+            KeyMaHoa = Convert.ToInt32(dr["ID"]);
+            Loai = Convert.ToByte(dr["Loai"]);
+        }
+    }
+}
